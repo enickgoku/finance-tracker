bc9841f49f7df44edd9a7bdb59accf6ebdb4b655
Nick Engelhardt <enickgoku2@gmail.com>
2022-12-02 12:37:52 -0500

    installed mongoose

---
+++ backend/src/db/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updataedAt: {
+    type: Date,
+    default: new Date(),
+  },
+})
+
+module.exports = mongoose.model('User', User)
